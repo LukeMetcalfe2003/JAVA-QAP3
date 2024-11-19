883e74deb90dbfb603d27b646c4d0b912b80213c
Luke Metcalfe <luke.metcalfe@keyin.com>
2024-11-19 14:42:43 -0330

    Added the Elipse class to problem 2

---
+++ problem3/Ellipse.java
@@ -0,0 +1,38 @@
+package problem3;
+
+public class Ellipse extends Shape{
+    private double majorAxis;
+    private double minorAxis;
+
+    public Ellipse(double firstAxis, double secondAxis) {
+        super("Ellipse");
+        if(firstAxis >= secondAxis){
+            this.majorAxis = firstAxis;
+            this.minorAxis = secondAxis;
+        } else {
+            this.majorAxis = secondAxis;
+            this.minorAxis = firstAxis;
+        }
+    }
+
+    @Override
+    public double getPerimeter() {
+        double diff = majorAxis - minorAxis;
+        if(diff == 0.0){
+            return Math.PI * 2 * majorAxis;
+        } else {
+            return Math.PI * Math.sqrt((2*(Math.pow(majorAxis, 2) + Math.pow(minorAxis, 2))) - Math.pow(diff, 2)/2);
+        }
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * majorAxis * minorAxis;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", major axis: " + String.format("%.2f", majorAxis) + ", minor axis: " + String.format("%.2f", minorAxis);
+    }
+    
+}
